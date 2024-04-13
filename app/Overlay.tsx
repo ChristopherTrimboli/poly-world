5517610120201ae14bf7b7abb61ea81725e50ec1
Christopher Trimboli <cjft@trimboli.io>
2024-04-13 07:54:43 -0600

    useEffect isMobile, window undefined fix

--- app/Overlay.tsx
+++ app/Overlay.tsx
@@ -1,9 +1,14 @@
 "use client";
 
 import { EcctrlJoystick } from "ecctrl";
+import { useEffect, useState } from "react";
 
 const Overlay = () => {
-  const isMobile = /iPhone|iPad|iPod|Android/i.test(window?.navigator?.userAgent);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    setIsMobile(/iPhone|iPad|iPod|Android/i.test(window?.navigator?.userAgent));
+  }, []);
 
   return <>{isMobile && <EcctrlJoystick buttonNumber={1} />}</>;
 };
