f3b2200cbe683f767f322e95a07a68d67da39bff
Christopher Trimboli <cjft@trimboli.io>
2024-04-14 22:58:54 -0600

    IndexedDB persistent land chunk storage

--- app/Terrain.tsx
+++ app/Terrain.tsx
@@ -1,7 +1,14 @@
 import { RigidBody } from "@react-three/rapier";
 import { useState, useEffect, useCallback, useRef } from "react";
-import { BoxGeometry, MeshPhongMaterial, SphereGeometry } from "three";
+import {
+  BoxGeometry,
+  MeshPhongMaterial,
+  SphereGeometry,
+  ObjectLoader,
+  Mesh,
+} from "three";
 import { Brush, SUBTRACTION, Evaluator } from "three-bvh-csg";
+import { openDB } from "idb";
 
 // sphere brush for CSG operations
 const sphereGeo = new SphereGeometry(2, 6, 6);
@@ -24,42 +31,85 @@ const Terrain = () => {
   const chunkKeys = useRef<string[]>([]);
 
   useEffect(() => {
-    setChunkRefs(
-      new Array(gridSize).fill(null).map((_, x) => {
-        return new Array(gridSize).fill(null).map((_, z) => {
-          const gridBrush = new Brush(
-            new BoxGeometry(
-              gridSpacing + 0.001,
-              gridSpacing + 0.001,
-              gridSpacing * 2,
-              1,
-              1,
-              1
-            )
-          );
-          gridBrush.material = new MeshPhongMaterial({
-            color: Math.random() * 0xffffff,
-            shininess: 5,
-            flatShading: true,
-            side: 2,
-          });
+    const loadChunks = async () => {
+      const db = await openDB("polyworld", 1);
 
-          gridBrush.position.set(
-            x * gridSpacing,
-            -gridSpacing,
-            z * gridSpacing
-          );
-          gridBrush.rotation.set(-Math.PI / 2, 0, 0);
+      for (let x = 0; x < gridSize; x++) {
+        for (let z = 0; z < gridSize; z++) {
+          // If the chunk is not in the database, create a new gridBrush
+          const dbChunk = await db.get("chunks", `${x}-${z}`);
+          if (!dbChunk) {
+            const gridBrush = new Brush(
+              new BoxGeometry(
+                gridSpacing + 0.001,
+                gridSpacing + 0.001,
+                gridSpacing * 2,
+                1,
+                1,
+                1
+              )
+            );
+            gridBrush.material = new MeshPhongMaterial({
+              color: Math.random() * 0xffffff,
+              shininess: 5,
+              flatShading: true,
+              side: 2,
+            });
 
-          chunkKeys.current[x + z] = Math.random().toString();
-          return gridBrush;
-        });
-      })
-    );
+            gridBrush.position.set(
+              x * gridSpacing,
+              -gridSpacing,
+              z * gridSpacing
+            );
+            gridBrush.rotation.set(-Math.PI / 2, 0, 0);
+
+            setChunkRefs((prev) => {
+              const newChunkRefs = [...prev];
+              newChunkRefs[x] = newChunkRefs[x] || [];
+              newChunkRefs[x][z] = gridBrush;
+
+              return newChunkRefs;
+            });
+            chunkKeys.current[x + z] = Math.random().toString();
+          } else {
+            // If the chunk is in the database, load it
+            const loader = new ObjectLoader();
+            const object3D = loader.parse(dbChunk);
+
+            object3D.traverse((child) => {
+              if (child instanceof Mesh) {
+                // Extract the BufferGeometry from the Mesh
+                const bufferGeometry = child.geometry;
+
+                // Create a new Brush with the BufferGeometry
+                const chunkRef = new Brush(bufferGeometry);
+                chunkRef.material = new MeshPhongMaterial({
+                  color: Math.random() * 0xffffff,
+                  shininess: 5,
+                  flatShading: true,
+                  side: 2,
+                });
+
+                setChunkRefs((prev) => {
+                  const newChunkRefs = [...prev];
+                  newChunkRefs[x] = newChunkRefs[x] || [];
+                  newChunkRefs[x][z] = chunkRef;
+
+                  return newChunkRefs;
+                });
+                chunkKeys.current[x + z] = Math.random().toString();
+              }
+            });
+          }
+        }
+      }
+    };
+
+    loadChunks();
   }, []);
 
   const onEditChunk = useCallback(
-    (e, x: number, z: number) => {
+    async (e, x: number, z: number) => {
       e.stopPropagation();
       const chunkRef = chunkRefs[x][z];
       chunkRef.updateMatrixWorld();
@@ -72,6 +122,16 @@ const Terrain = () => {
         sphereBrush,
         SUBTRACTION
       );
+
+      const chunkJSON = newResultCSG.toJSON();
+
+      const db = await openDB("polyworld", 1, {
+        upgrade(db) {
+          db.createObjectStore("chunks");
+        },
+      });
+
+      await db.put("chunks", chunkJSON, `${x}-${z}`);
 
       setChunkRefs((prev) => {
         const newChunkRefs = [...prev];
